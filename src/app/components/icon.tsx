5d35f9cf7137c37f149a20e75eebf865c91ee33e
Mohamed Amjar <amjar.mohamed.rh@gmail.com>
2024-10-27 01:48:48 +0100

    chore (icons): update social media icons 2

--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,21 +1,21 @@
-// components/Icon.js
-import * as Icons from 'lucide-react';
+// // components/Icon.js
+// import * as Icons from 'lucide-react';
 
-const MyIcon = ({ iconName, size = 24, color = 'currentColor' }) => {
-  // Dynamically choose the icon from the lucide-react icons library
-  const LucideIcon = Icons[iconName];
+// const MyIcon = ({ iconName, size = 24, color = 'currentColor' }) => {
+//   // Dynamically choose the icon from the lucide-react icons library
+//   const LucideIcon = Icons[iconName];
 
-  // If the icon doesn't exist, return null or some fallback UI
-  if (!LucideIcon) {
-    return <span>Icon not found</span>;
-  }
+//   // If the icon doesn't exist, return null or some fallback UI
+//   if (!LucideIcon) {
+//     return <span>Icon not found</span>;
+//   }
 
-  return (
-    <LucideIcon
-      size={size}
-      color={color}
-    />
-  );
-};
+//   return (
+//     <LucideIcon
+//       size={size}
+//       color={color}
+//     />
+//   );
+// };
 
-export default MyIcon;
+// export default MyIcon;
